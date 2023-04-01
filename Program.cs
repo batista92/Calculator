31077d3c8c8472021f72c5772ddc4ac5046643c4
Batista92 <bruno.2.s@live.com>
2023-04-01 19:09:21 -0300

    Finalização do projeto

--- Program.cs
+++ Program.cs
@@ -1,38 +1,65 @@
-﻿int num1 = 0; int num2 = 0;
+﻿using Calculator;
 
-Console.WriteLine("Calculadora de Console em C#\r");
-Console.WriteLine("----------------------------\n");
+bool endApp = false;
 
-Console.WriteLine("Digite um número. e pressione ENTER");
-num1 = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Digite outro número, e pressione ENTER");
-num2 = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine(
-    @"Escolha uma das opções da lista abaixo: 
-    a - Adição
-    s - Subtração
-    m - Multiplicação
-    d - Divisão"
-);
-Console.Write("Digite uma opção: ");
-
-switch (Console.ReadLine())
+while (!endApp)
 {
-    case "a":
-        Console.WriteLine($"Seu Resultado: {num1} + {num2} = " + (num1 + num2));
-        break;
-    case "s":
-        Console.WriteLine($"Seu Resultado: {num1} - {num2} = " + (num1 - num2));
-        break;
-    case "m":
-        Console.WriteLine($"Seu Resultado: {num1} * {num2} = " + (num1 * num2));
-        break;
-    case "d":
-        Console.WriteLine($"Seu Resultado: {num1} / {num2} = " + (num1 / num2));
-        break;
-}
+    Console.Clear();
+    Console.WriteLine("Calculadora de Console em C#\r");
+    Console.WriteLine("----------------------------\n");
+    string numInput1 = "";
+    string numInput2 = "";
+    double result = 0;
 
-Console.WriteLine("Pressione qualquer tecla para fechar a Calculadora...");
-Console.ReadKey();
+    Console.WriteLine("Digite um número. e pressione ENTER");
+    numInput1 = Console.ReadLine();
+
+    double cleanNum1 = 0;
+    while (!double.TryParse(numInput1, out cleanNum1))
+    {
+        Console.Write("Entrada invalida! Por favor digite um valor numérico: ");
+        numInput1 = Console.ReadLine();
+    }
+
+    Console.WriteLine("Digite outro número. e pressione ENTER");
+    numInput2 = Console.ReadLine();
+
+    double cleanNum2 = 0;
+    while (!double.TryParse(numInput2, out cleanNum2))
+    {
+        Console.Write("Entrada invalida! Por favor digite um valor numérico: ");
+        numInput2 = Console.ReadLine();
+    }
+
+    Console.WriteLine(
+        @"Escolha uma das opções da lista abaixo: 
+            a - Adição
+            s - Subtração
+            m - Multiplicação
+            d - Divisão"
+    );
+    Console.Write("Digite uma opção: ");
+    string op = Console.ReadLine();
+
+    try
+    {
+        result = Calculator.Calculator.DoOperation(cleanNum1, cleanNum2, op);
+        if (double.IsNaN(result))
+        {
+            Console.WriteLine("Esta operação resultará em um erro matemático.\n");
+        }
+        else
+        {
+            Console.WriteLine("Seu resultado: {0:0.##}\n", result);
+        }
+
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine("Ah não! Ocorreu uma exceção ao tentar fazer as contas.\n - Detalhes: " + e.Message);
+    }
+
+    Console.WriteLine("Pressione qualquer tecla para fechar a Calculadora...");
+    Console.ReadKey();
+}
+return;
