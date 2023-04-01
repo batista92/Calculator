43ef92975a0bc994c4163f1501f908fdcf638b84
Batista92 <bruno.2.s@live.com>
2023-04-01 17:49:01 -0300

    Primeira Etapa do tutorial

--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int num1 = 0; int num2 = 0;
+
+Console.WriteLine("Calculadora de Console em C#\r");
+Console.WriteLine("----------------------------\n");
+
+Console.WriteLine("Digite um número. e pressione ENTER");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite outro número, e pressione ENTER");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(
+    @"Escolha uma das opções da lista abaixo: 
+    a - Adição
+    s - Subtração
+    m - Multiplicação
+    d - Divisão"
+);
+Console.Write("Digite uma opção: ");
+
+switch (Console.ReadLine())
+{
+    case "a":
+        Console.WriteLine($"Seu Resultado: {num1} + {num2} = " + (num1 + num2));
+        break;
+    case "s":
+        Console.WriteLine($"Seu Resultado: {num1} - {num2} = " + (num1 - num2));
+        break;
+    case "m":
+        Console.WriteLine($"Seu Resultado: {num1} * {num2} = " + (num1 * num2));
+        break;
+    case "d":
+        Console.WriteLine($"Seu Resultado: {num1} / {num2} = " + (num1 / num2));
+        break;
+}
+
+Console.WriteLine("Pressione qualquer tecla para fechar a Calculadora...");
+Console.ReadKey();
